cdbed5662bee98d5e100888ccff49c86916aae7c
Surendra Oraon <osuren155@gmail.com>
2026-01-22 23:06:34 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ st.markdown(
 st.markdown("<br>", unsafe_allow_html=True)
 
 # ---------- IMAGE ----------
-image_path = ("Q3.jpg")  
+image_path = ("Q3.jpg" , width=900)  
 
 if os.path.exists(image_path):
     with open(image_path, "rb") as img_file:
